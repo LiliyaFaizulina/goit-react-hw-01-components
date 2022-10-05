98d8046d0791372a978f63bda978a809e77afdce
Лілія <lily.fed85@gmail.com>
2022-10-05 17:04:55 +0300

    add proptypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -22,5 +22,11 @@ export function Profile({ user:{username, tag, location, avatar, stats} }) {
 }
 
 Profile.propTypes = {
-    user: PropTypes.shape().isRequired
+    user: PropTypes.shape({
+        avatar: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+        tag: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
+        stats: PropTypes.object.isRequired
+    }).isRequired
 };
